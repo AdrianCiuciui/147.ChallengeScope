8c31ff2b4dc05354699284b451308334ac8cd135
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-10-30 21:11:13 +0200

    bug fixes

--- src/com/company/X.java
+++ src/com/company/X.java
@@ -11,13 +11,12 @@ public class X {
     private void x () {
         int x = 0;
         while (x < 12) {
-            System.out.println("1 times " + this.x + " equals " + (x+1) * this.x);
+            System.out.println((x+1) + " times " + this.x + " equals " + (x+1) * this.x);
             x++;
         }
     }
 
     public void print () {
         x();
-        System.out.println("input is " + this.x);
     }
 }
